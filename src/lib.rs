db4d719afcf9aaa2c80b7ee0252e44107842c39f
Ansel Horn <dev@cahorn.net>
2025-02-08 17:04:53 -0500

    Add CLI for selecting days/parts

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
 pub mod day01;
+
+pub const DAYS: [[fn(&str) -> i32; 2]; 1] = [[day01::part1, day01::part2]];
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use aoc24::day01;
+use std::env;
 use std::io;
 use std::io::Read;
 
 fn main() -> io::Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    println!("{}", day01::part1(&input));
+    let mut args = env::args().skip(1);
+    let day: usize = args
+        .next()
+        .expect("No day provided")
+        .parse()
+        .expect("Could not read day");
+    let part: usize = args
+        .next()
+        .expect("No part provided")
+        .parse()
+        .expect("Could not parse part");
+    println!("{}", aoc24::DAYS[day][part](&input));
     Ok(())
 }
